d4d0a8d086cae834ef4261940664152f868a18ad
Lee <lee@leeharkness.com>
2024-02-25 12:46:35 -0600

    Initial UI

---
+++ src/main/java/com/leeharkness/orderpoc/OrderpocApplication.java
@@ -0,0 +1,13 @@
+package com.leeharkness.orderpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderpocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderpocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leeharkness/orderpoc/OrderpocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leeharkness.orderpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderpocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
